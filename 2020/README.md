009a83db4f6f25617a77dfd03bda84f89feff25b
dinesh-k-natarajan <dineshkrishna.natarajan@gmail.com>
2020-12-09 15:57:26 +0100

    solved 2020 day9

--- 2020/README.md
+++ 2020/README.md
@@ -2,6 +2,12 @@
 
 Language: Python
 
+| Day | Solution |
+|-----------|-----------|
+| [Day 1: Rocket Repair](https://adventofcode.com/2020/day/1)|   `Part 1 Solution =  776064 `       |  |                                                     |   `Part 2 Solution =  6964490 `      |
+|                                                     |           |
+|                                                     |           |
+
 ## Answers for the challenges
 1. [Rocket Repair](https://adventofcode.com/2020/day/1)
 ```bash
@@ -50,4 +56,10 @@ Part 2 Solution =  38426
 $ python 8.py
 Part 1 Solution =  2025
 Part 2 Solution =  2001
+```
+9. [Encoding Error](https://adventofcode.com/2020/day/9)
+```bash
+$ python 9.py
+Part 1 Solution =  375054920
+Part 2 Solution =  54142584
 ```
---
+++ 2020/day9/9.py
@@ -0,0 +1,60 @@
+import pytest
+from itertools import combinations
+
+def get_inputs( filename ):
+    """
+    The input file has a number in each line. 
+    This function returns the numbers as a list.
+    """
+    with open( filename, 'r') as input_file:
+        inputs = [ int(item) for item in input_file.read().splitlines() ]
+    return inputs
+
+def find_invalid( data, preamble ):
+    """
+    From the given list of numbers, each number after the index 'preamble' should 
+    be a sum of any two of the previous 'preamble' numbers. This function returns
+    the first invalid number that fails this property. 
+    """
+    for loc in range( preamble, len(data) ):
+        current_num = data[ loc ] 
+        subset = data[ loc-preamble : loc ]
+        pairs  = combinations( subset, 2 )
+        flag   = any([ current_num == sum(pair) for pair in pairs ])
+        if not flag:
+            return current_num
+
+def find_limits( data, preamble ):
+    """
+    After finding the invalid number from find_invalid function, the goal is to
+    find a contiguous range of arbitrary length > 2 in the data that adds upto 
+    the invalid number. This function returns the sum( min, max )
+    """
+    invalid     = find_invalid( data, preamble )
+    invalid_loc = data.index( invalid )
+    subset      = data[ :invalid_loc ]
+    len_range   = 2
+    found_range = False
+    while not found_range:
+        ranges = [ [ *subset[ idx:idx+len_range ]] for idx in range(len(subset)-len_range+1) ]
+        for range_ in ranges:
+            if sum( range_ ) == invalid:
+                limits = range_
+                found_range = True
+        len_range += 1
+    return min( limits ) + max( limits ) 
+
+@pytest.mark.parametrize( 'test_input, expected', [ ('9.example', 127) ] )
+def test_part1( test_input, expected ):
+    assert find_invalid( get_inputs(test_input), preamble=5 ) == expected
+
+@pytest.mark.parametrize( 'test_input, expected', [ ('9.example', 62) ] )
+def test_part1( test_input, expected ):
+    assert find_limits( get_inputs(test_input), preamble=5 ) == expected
+
+def main():
+    print('Part 1 Solution = ', find_invalid( get_inputs('9.in'), preamble=25 ) )
+    print('Part 2 Solution = ', find_limits(  get_inputs('9.in'), preamble=25 ) )
+
+if __name__ == '__main__':
+    exit( main())

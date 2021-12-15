01e5d27c59f56f34fffa8143512f58f10d263ebd
dinesh-k-natarajan <dineshkrishna.natarajan@gmail.com>
2021-12-15 12:13:43 +0100

    improved readability 2021 day 08

--- 2021/08/8.py
+++ 2021/08/8.py
@@ -6,6 +6,7 @@ def get_input( filename ):
         return [ [digit.split(' ') for digit in line.split(' | ')] for line in lines ]         
 
 def count_1478( entries ):
+    # Unique segment lengths of digits 1,4,7,8
     segments_1478 = [ 2, 4, 3, 7 ]
     count = 0
     for entry in entries:
@@ -13,33 +14,54 @@ def count_1478( entries ):
     return count
 
 def decode_values( entry ):
+    # Unique segment lengths of digits 1,4,7,8
     segments_1478 = [ 2, 4, 3, 7 ]
+    # Mapping between length of segments and the possible digits
     lengths = { 2:'1', 3:'7', 4:'4', 5:{'2','3','5'}, 6:{'0','6','9'}, 7:'8' }  
+    
     signal_patterns, encoded_outputs = entry
+    # Store the decoded values of the signal pattern
     decoded_values = signal_patterns.copy()
+    # Store the digit to pattern mapping
     legend = dict()
+    # Store the decoded output values
     decoded_outputs = []
+
+    # Identify the digits 1,4,7,8 which have unique length of segments
     for i, encoded_pattern in enumerate( signal_patterns ):
         if len(encoded_pattern) in segments_1478:
             decoded_values[i] = lengths[ len(encoded_pattern) ]
             legend[ decoded_values[i] ] = set( encoded_pattern )
-        else: decoded_values[i] = lengths[ len(encoded_pattern) ]  
+        else: 
+            decoded_values[i] = lengths[ len(encoded_pattern) ]  
+    
+    # Identify the remaining digits using logic described in 8_logic.png
     for i, encoded_pattern in enumerate( signal_patterns ):
         while not isinstance( decoded_values[i], str ):
+            # Identify digits [2,3,5]
             if decoded_values[i].issubset( lengths[5] ):
                 flag_3 = legend['7'].issubset( set(encoded_pattern) )
                 flag_5 = len( set(encoded_pattern).intersection( legend['4'] ) ) == 3
-                if flag_3: decoded_values[i] = '3'
-                elif not flag_3 and flag_5: decoded_values[i] = '5'
-                elif not flag_3 and not flag_5: decoded_values[i] = '2'
+                if flag_3: 
+                    decoded_values[i] = '3'
+                elif flag_5: 
+                    decoded_values[i] = '5'
+                else: 
+                    decoded_values[i] = '2'
                 legend[ decoded_values[i] ] = set( encoded_pattern )
+            # Identify digits [0,6,9]
             elif decoded_values[i].issubset( lengths[6] ):
                 flag_9 = legend['4'].issubset( set(encoded_pattern) )
                 flag_0 = legend['7'].issubset( set(encoded_pattern) )
-                if flag_9: decoded_values[i] = '9'
-                elif not flag_9 and flag_0: decoded_values[i] = '0'
-                elif not flag_9 and not flag_0: decoded_values[i] = '6'
+                if flag_9: 
+                    decoded_values[i] = '9'
+                elif flag_0: 
+                    decoded_values[i] = '0'
+                else: 
+                    decoded_values[i] = '6'
                 legend[ decoded_values[i] ] = set( encoded_pattern )
+    
+    # Using the legend, decode the encoded_outputs in the entries
     for encoded_output in encoded_outputs:
         decoded_outputs += [ key for key, value in legend.items() if set(encoded_output) == value ]
     return int( ''.join(decoded_outputs) )

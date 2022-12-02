5556ebda68eba58e668b8efb372dee8cb59e4227
dinesh-k-natarajan <dineshkrishna.natarajan@gmail.com>
2022-12-02 14:31:25 +0530

    cleaner 2020 day 2 solution

--- 2022/python/02/2.py
+++ 2022/python/02/2.py
@@ -2,81 +2,52 @@ import pytest
 
 def get_input( filename ):
     with open(filename,'r') as input_file:
-        return [line.split(' ') for line in input_file.read().splitlines()]
+        return [''.join(line.split(' ')) for line in input_file.read().splitlines()]
 
-def compute_RPS_results( games ): 
-    """ 
-    list of games with each game containing [opponent_shape, your_shape]
+def decode_outcome(games):
     """
-    results = []
-    for game in games:
-        if game[1] == game[0]: results.append('D')
-        elif game[1]=='R':
-            if game[0]=='S': results.append('W') # Rock beats scissors
-            elif game[0]=='P': results.append('L') # Rock loses to paper
-        elif game[1]=='P':
-            if game[0]=='R': results.append('W') # Paper beats rock
-            elif game[0]=='S': results.append('L') # Paper loses to scissors
-        elif game[1]=='S':
-            if game[0]=='P': results.append('W') # Scissors beats paper
-            elif game[0]=='R': results.append('L') # Scissors loses to rock
-    return results
+    Decide the outcomes of game w.r.t Player 2
+    """
+    possible_outcomes = {
+        'W': ['RP', 'PS', 'SR'],
+        'D': ['RR', 'PP', 'SS'],
+        'L': ['RS', 'PR', 'SP']
+    }
+    return [[key for key, value in possible_outcomes.items() if game in value][0] for game in games]
+
+SHAPES_TO_POINTS  = {'R':1, 'P':2, 'S':3}
+RESULTS_TO_POINTS = {'W':6, 'D':3, 'L':0}
 
 def count_score_part1( games ):
     """
     games is a list of lists with each list containing [opponent_shape, your_shape]
-    Strategy guide:
-    Points for shapes: {'R':1, 'P':2, 'S':3}
-    Points for results: {'W':6, 'D':3, 'L':0}
+    ABC is opponent's RPS, XYZ is my RPS
     """
-    shapes_to_RPS = {'A':'R', 'B':'P', 'C':'S', 'X':'R', 'Y':'P', 'Z':'S'}
-    translated_games = [[shapes_to_RPS[game[0]], shapes_to_RPS[game[1]]] for game in games]
-    results = compute_RPS_results(translated_games)
-    shapes_to_points = {'R':1, 'P':2, 'S':3}
-    results_to_points = {'W':6, 'D':3, 'L':0}
-    points_from_shapes = [shapes_to_points[game[1]] for game in translated_games]
-    points_from_results = [results_to_points[result] for result in results]
-    return sum([p1+p2 for p1,p2 in zip(points_from_shapes, points_from_results)])
-
-def decode_games( games ):
+    translator = {'A':'R', 'B':'P', 'C':'S', 'X':'R', 'Y':'P', 'Z':'S'}
+    translated_games = [''.join([translator[game[0]], translator[game[1]]]) for game in games]
+    return sum([SHAPES_TO_POINTS[game[1]] for game in translated_games] 
+            + [RESULTS_TO_POINTS[result] for result in decode_outcome(translated_games)])
+    
+def decode_choice( games ): 
     """
-    games is a list of lists with each list containing [opponent_shape, desired_result]
-    xyz_to_results = {'X':'L', 'Y':'D', 'Z': 'W'}
-    Compute your shapes according to desired results.
+    Decode the choice of Player 2 based on Player 1 and desired outcome
     """
-    shapes_to_RPS = {'A':'R', 'B':'P', 'C':'S'}
-    xyz_to_results = {'X':'L', 'Y':'D', 'Z': 'W'}
-    translated_games = [[shapes_to_RPS[game[0]],xyz_to_results[game[1]]] for game in games]
-    results = [xyz_to_results[game[1]] for game in games]
-    decoded_games = []
-    for game in translated_games:
-        if  game[1] == 'D': decoded_game = [game[0], game[0]] # copycat
-        elif game[1] == 'W':
-            if game[0] == 'R': decoded_game = [game[0], 'P'] # P beats R
-            elif game[0] == 'P': decoded_game = [game[0], 'S'] # S beats P
-            elif game[0] == 'S': decoded_game = [game[0], 'R'] # R beats S
-        elif game[1] == 'L':
-            if game[0] == 'R': decoded_game = [game[0], 'S'] # S loses to R
-            elif game[0] == 'P': decoded_game = [game[0], 'R'] # R loses to P
-            elif game[0] == 'S': decoded_game = [game[0], 'P'] # P loses to S
-        decoded_games.append(decoded_game)
-    return results, decoded_games
+    possible_choices = {
+        'R': ['SW', 'RD', 'PL'],
+        'P': ['RW', 'PD', 'SL'],
+        'S': ['PW', 'SD', 'RL'],
+    }
+    return [[key for key, value in possible_choices.items() if game in value][0] for game in games]
 
 def count_score_part2( games ):
     """ 
     games is a list of lists with each list containing [opponent_shape, desired_result]
     xyz_to_results = {'X':'L', 'Y':'D', 'Z': 'W'}
-    Strategy guide remains the same:
-    Strategy guide:
-    Points for shapes: {'R':1, 'P':2, 'S':3}
-    Points for results: {'W':6, 'D':3, 'L':0}
     """
-    results, decoded_games = decode_games( games )
-    shapes_to_points = {'R':1, 'P':2, 'S':3}
-    results_to_points = {'W':6, 'D':3, 'L':0}
-    points_from_shapes = [shapes_to_points[game[1]] for game in decoded_games]
-    points_from_results = [results_to_points[result] for result in results]
-    return sum([p1+p2 for p1,p2 in zip(points_from_shapes, points_from_results)])
+    translator = {'A':'R', 'B':'P', 'C':'S', 'X':'L', 'Y':'D', 'Z':'W'}
+    translated_games = [''.join([translator[game[0]], translator[game[1]]]) for game in games]
+    return sum([SHAPES_TO_POINTS[game] for game in decode_choice(translated_games)] 
+            + [RESULTS_TO_POINTS[game[1]] for game in translated_games])
 
 @pytest.mark.parametrize( 'test_input,expected', [ ('2.example', 15) ] )
 def test_part1( test_input, expected ):

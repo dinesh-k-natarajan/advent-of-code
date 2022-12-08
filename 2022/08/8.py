26ad03728ceb14e9715d702fccbeef1cd42c2e07
dinesh-k-natarajan <dineshkrishna.natarajan@gmail.com>
2022-12-08 13:33:03 +0530

    cleaned up 2022 day 8, better naming

--- 2022/08/8.py
+++ 2022/08/8.py
@@ -5,69 +5,65 @@ def parse_input( filename ):
     with open(filename,'r') as input_file:
         return [[int(tree) for tree in line] for line in input_file.read().splitlines()]
 
-def compute_1( trees ):
+def count_visible_trees( trees ):
     grid_dims = [len(trees),len(trees[0])]
-    ext_visible = []
-    int_visible = []
     # trees on grid boundary are visible from outside
-    for i in range(grid_dims[0]):
-        if i in [0, grid_dims[0]-1]:
-            ext_visible += trees[i]
-        else:
-            ext_visible += [trees[i][0], trees[i][grid_dims[1]-1]]
+    n_boundary = 2*(grid_dims[0]+grid_dims[1]-2)
     # internal trees that are shorter than others are visible from the outside
+    int_visible = []
     for i in range(1,grid_dims[0]-1):
         for j in range(1,grid_dims[1]-1):
             tree = trees[i][j]
             # is the tree shorter than all other trees from itself to the edge?
-            top = all(t < tree for t in [row[j] for row in trees[:i]])
-            bottom = all(t < tree for t in [row[j] for row in trees[i+1:]])
-            left = all(t < tree for t in trees[i][:j])
-            right = all(t < tree for t in trees[i][j+1:])
+            top_flag    = all(t < tree for t in [row[j] for row in trees[:i]])
+            bottom_flag = all(t < tree for t in [row[j] for row in trees[i+1:]])
+            left_flag   = all(t < tree for t in trees[i][:j])
+            right_flag  = all(t < tree for t in trees[i][j+1:])
             # is it visible from any of the 4 directions?
-            if top | bottom | left | right:
+            if top_flag | bottom_flag | left_flag | right_flag:
                 int_visible.append(tree)
-    return len(ext_visible+int_visible)
+    return n_boundary+len(int_visible)
 
-def get_unblocked_trees(tree,view):
+def count_unblocked( tree, view ):
     # find number of unblocked trees from the tree in a specific view
     if all(t<tree for t in view): # if no trees are >= tree
-        unblocked_trees = len(view)
+        n_unblocked = len(view)
     else:
         # from the tree, you can see upto the first tallest tree
         taller_trees = [view.index(t) for t in view if t>=tree]
-        unblocked_trees = taller_trees[0]+1 # start index from 1
-    return unblocked_trees
+        n_unblocked = taller_trees[0]+1 # start index from 1
+    return n_unblocked
 
-def compute_2( trees ):
+def compute_max_score( trees ):
     grid_dims = [len(trees),len(trees[0])]
     scenic_scores = []
     # ignoring trees on boundary (since their score in atleast one direction = 0)
     for i in range(1,grid_dims[0]-1):
         for j in range(1,grid_dims[1]-1):
             tree = trees[i][j]
-            top = [row[j] for row in trees[:i]][::-1] # order reverseed, from tree POV
-            bottom = [row[j] for row in trees[i+1:]]
-            left = trees[i][:j][::-1] # order reversed, from tree POV
-            right = trees[i][j+1:]
+            # collect list of trees in each direction from tree, with the tree as origin
+            top_trees    = [row[j] for row in trees[:i]][::-1] # reversed, from origin
+            bottom_trees = [row[j] for row in trees[i+1:]]
+            left_trees   = trees[i][:j][::-1] # reversed, from origin
+            right_trees  = trees[i][j+1:]
             score = 1
-            for view in [top,bottom,left,right]:
-                score *= get_unblocked_trees(tree,view)
+            for view in [top_trees,bottom_trees,left_trees,right_trees]:
+                score *= count_unblocked(tree,view)
             scenic_scores.append(score)
     return max(scenic_scores)
 
 @pytest.mark.parametrize( 'test_input,expected', [ ('8.example', 21) ] )
 def test_part1( test_input, expected ):
-    assert compute_1( parse_input(test_input) ) == expected
+    assert count_visible_trees( parse_input(test_input) ) == expected
 
 @pytest.mark.parametrize( 'test_input,expected', [ ('8.example', 8) ] )
 def test_part2( test_input, expected ):
-    assert compute_2( parse_input(test_input) ) == expected
+    assert compute_max_score( parse_input(test_input) ) == expected
 
 def main():
     start_time = time.perf_counter()
-    print('Part 1 Solution = ', compute_1( parse_input('8.in') ))
-    print('Part 2 Solution = ', compute_2( parse_input('8.in') ))
+    print('Part 1 Solution = ', count_visible_trees( parse_input('8.in') ))
+    print('Part 2 Solution = ', compute_max_score(   parse_input('8.in') ))
     end_time = time.perf_counter()
     print(f'Execution took {(end_time-start_time):.5f} s')
 

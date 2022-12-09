12e58c60621122fe64782945baf3b945a80aad93
dinesh-k-natarajan <dineshkrishna.natarajan@gmail.com>
2022-12-09 13:12:05 +0530

    cleaned up 2022 day 09

--- 2022/09/09.py
+++ 2022/09/09.py
@@ -16,6 +16,7 @@ DIAGONAL  = {'Q1':(1,1),'Q2':(-1,1),'Q3':(-1,-1),'Q4':(1,-1)}
 def check_separation( H, T ):
     # T should be in contact with H either adjacently or diagonally
     if sum(item**2 for item in [h-t for h,t in zip(H,T)])<=2:
+        # |difference| in [(0,0), (0,1), (1,0), (1,1)] => in contact
         return False
     else:
         return True
@@ -24,60 +25,52 @@ def move_tail( H, T ):
     # Assumed that H,T are separated and T must be moved
     diff = tuple(h-t for h,t in zip(H,T))
     # if H,T are in the same row or column
-    if diff[0]*diff[1] == 0: 
+    if diff[0]*diff[1] == 0:
+        # since actions are applied step-by-step, max(diff) is usually 2
+        # find unit vector from diff to move T in that direction 
         move = tuple(item//2 for item in diff)
         T = tuple(t+m for t,m in zip(T,move))
-    # if h,t aren't in the same row or column
+    # if H,T aren't in the same row or column
     else:
+        # find which quadrant the diff lies in
         quad = QUADRANTS[tuple(item>0 for item in diff)]
+        # move T diagonally to that quadrant
         T = tuple(t+d for t,d in zip(T, DIAGONAL[quad]))
     return T 
 
-def compute_1( actions ):
-    origin = (0,0)
-    H_path = [origin]
-    T_path = [origin]
-    for action in actions:
-        dir, dist = action
-        H = H_path[-1]
-        T = T_path[-1]
-        for _ in range(dist):
-            H = tuple(h+direc for h,direc in zip(H,DIRECTION[dir]))
-            H_path.append(H)
-            if check_separation(H,T):
-                T = move_tail(H,T)
-                T_path.append(T)
-    return len(set(T_path))
-
-def compute_2( actions ):
+def count_visited_positions( actions, n_knots=2 ):
     origin=(0,0)
-    # Head rope and 9 tail ropes
-    ropes = {rope:[origin] for rope in range(10)}
+    # all knots start at the origin
+    knots = {rope:[origin] for rope in range(n_knots)}
     for action in actions:
         dir, dist = action
-        H = ropes[0][-1]
+        # First knot is the Head
+        H = knots[0][-1]
+        # Move the head step-by-step based on action
         for _ in range(dist):
             H = tuple(h+direc for h,direc in zip(H,DIRECTION[dir]))
-            ropes[0].append(H)
-            # Check separation and move tail of each 2 ropes
-            for i in range(1,10):
-                if check_separation(ropes[i-1][-1],ropes[i][-1]):
-                    T = move_tail(ropes[i-1][-1],ropes[i][-1])
-                    ropes[i].append(T)
-    return len(set(ropes[9]))
+            knots[0].append(H)
+            # Looping over all non-head knots
+            for i in range(1,n_knots):
+                # Check separation of previous and next knots
+                if check_separation(knots[i-1][-1],knots[i][-1]):
+                    # If separated, move tail knot based on previous knot
+                    T = move_tail(knots[i-1][-1],knots[i][-1])
+                    knots[i].append(T)
+    return len(set(knots[n_knots-1]))
 
 @pytest.mark.parametrize( 'test_input,expected', [ ('09.example', 13) ] )
 def test_part1( test_input, expected ):
-    assert compute_1( parse_input(test_input) ) == expected
+    assert count_visited_positions( parse_input(test_input) ) == expected
 
 @pytest.mark.parametrize( 'test_input,expected', [ ('09.example', 1), ('09.example2', 36) ] )
 def test_part2( test_input, expected ):
-    assert compute_2( parse_input(test_input) ) == expected
+    assert count_visited_positions( parse_input(test_input), n_knots=10 ) == expected
 
 def main():
     start_time = time.perf_counter()
-    print('Part 1 Solution = ', compute_1( parse_input('09.in') ))
-    print('Part 2 Solution = ', compute_2( parse_input('09.in') ))
+    print('Part 1 Solution = ', count_visited_positions( parse_input('09.in') ))
+    print('Part 2 Solution = ', count_visited_positions( parse_input('09.in'), n_knots=10 ))
     end_time = time.perf_counter()
     print(f'Execution took {(end_time-start_time):.5f} s')
 

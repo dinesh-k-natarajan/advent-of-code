e39f367e5b55df8d8f16bd4038ed36c39586a3fe
dinesh-k-natarajan <dineshkrishna.natarajan@gmail.com>
2022-12-10 13:47:08 +0530

    updated formatting

--- 2022/10/10.py
+++ 2022/10/10.py
@@ -1,11 +1,11 @@
 import time
 import pytest
 
-def parse_input( filename ):
+def parse_input(filename):
     with open(filename,'r') as input_file:
         return [line.split(' ') for line in input_file.read().splitlines()]
 
-def compute_signal_strengths( instructions ):
+def compute_signal_strengths(instructions):
     # find sum of signal strengths DURING (not after) the specified cycles
     X = [1]
     for instruction in instructions:
@@ -18,12 +18,12 @@ def compute_signal_strengths( instructions ):
             # add value to previous signal in second cycle
             X.append(X[-1]+int(instruction[1]))
     cycles = [20,60,100,140,180,220]
-    return sum(cycle*X[cycle-1] for cycle in cycles) # NOTE: X[cycle-1] -> DURING
+    return sum(cycle*X[cycle-1] for cycle in cycles) #NOTE: X[cycle-1] -> DURING
 
-def draw_pixel( CRT, sprite_pos, cycle):
+def draw_pixel(CRT,sprite_pos,cycle):
     # This function draws a pixel with either # or . and increments cycle
     ## get row,col to draw on CRT
-    row, col = cycle//40, cycle%40
+    row,col = cycle//40,cycle%40
     # vertical pos of sprite is purely determined by current cycle
     sprite_pos[0] = row
     if sprite_pos[1] in list(range(col-1,col+2)):
@@ -31,9 +31,9 @@ def draw_pixel( CRT, sprite_pos, cycle):
         CRT[row][col]='#'
     else:
         CRT[row][col]='.'
-    return CRT, sprite_pos, cycle+1
+    return CRT,sprite_pos,cycle+1
     
-def decode_CRT( instructions ):
+def decode_CRT(instructions):
     # CRT is a 2D grid of 40 columns and 6 rows starting from top left
     CRT = [['0' for _ in range(40)] for _ in range(6)]
     # The sprite is ### with location [row,col] in CRT
@@ -51,18 +51,18 @@ def decode_CRT( instructions ):
             sprite_pos[1] += int(instruction[1])
     return '\n'.join([''.join(row) for row in CRT])
 
-@pytest.mark.parametrize( 'test_input,expected', [ ('10.example', 13140) ] )
-def test_part1( test_input, expected ):
-    assert compute_signal_strengths( parse_input(test_input) ) == expected
+@pytest.mark.parametrize('test_input,expected', [('10.example',13140)])
+def test_part1(test_input,expected):
+    assert compute_signal_strengths(parse_input(test_input)) == expected
 
-@pytest.mark.parametrize( 'test_input,expected', [ ('10.example', '10.ex_solution') ] )
-def test_part2( test_input, expected ):
-    assert decode_CRT( parse_input(test_input) ) == open(expected,'r').read()
+@pytest.mark.parametrize('test_input,expected', [('10.example','10.ex_solution')])
+def test_part2(test_input,expected):
+    assert decode_CRT(parse_input(test_input)) == open(expected,'r').read()
 
 def main():
     start_time = time.perf_counter()
-    print('Part 1 Solution =',compute_signal_strengths( parse_input('10.in') ))
-    print('Part 2 Solution =\n{0}'.format( decode_CRT( parse_input('10.in') )))
+    print(f"Part 1 Solution = {compute_signal_strengths(parse_input('10.in'))}")
+    print(f"Part 2 Solution = \n{decode_CRT(parse_input('10.in'))}")
     end_time = time.perf_counter()
     print(f'Execution took {(end_time-start_time):.5f} s')
 

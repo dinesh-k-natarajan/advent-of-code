bbe7048b1fd08336e0ed2a77499011736441cc72
dinesh-k-natarajan <dineshkrishna.natarajan@gmail.com>
2022-12-10 10:23:23 +0530

    updated copy template script

--- 2022/copy_template.sh
+++ 2022/copy_template.sh
@@ -6,5 +6,5 @@ do
     mv "$file" "${file/template./$DAY.}"
 done
 sed -i "s/1.example/$DAY.example/" $DAY.py
-sed -i "s/1.in/$DAY.in/" $DAY.py
+sed -i "s/1.in/$DAY.example/" $DAY.py
 sed -i "s/1/$DAY/" README.md
